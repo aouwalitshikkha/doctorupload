66d140f24c96b27ae3aa801ba5790a76e21ef6de
aouwalitshikkha <aouwalitshikkha@gmail.com>
2026-06-29 15:50:07 +0600

    switch db to sqlite3 for local dev

--- settings.py
+++ settings.py
@@ -82,15 +82,8 @@ WSGI_APPLICATION = 'doctorsWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'nabeendoctor',
-        'USER': 'NabeenDoc',
-        'PASSWORD': '3DJmaXjzfHLjwcFa',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
